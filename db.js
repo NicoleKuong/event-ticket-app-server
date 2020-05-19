0cf53da979d481c74417c90f36a74d6a02cd8062
NicoleKuong <nicolekuong@gmail.com>
2020-05-19 11:44:06 +0200

    minor bug

--- db.js
+++ db.js
@@ -4,6 +4,7 @@ const dataBaseURL =
   process.env.DATABASE_URL ||
   "postgres://postgres:secret@localhost:5432/postgres";
 
+console.log("process.env.DATABASE_URL", process.env.DATABASE_URL);
 const db = new Sequelize(dataBaseURL);
 
 db.sync({ force: false })
